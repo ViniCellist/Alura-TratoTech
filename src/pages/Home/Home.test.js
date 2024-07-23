e6b101f7ae1d6e3ae97c7fadc2fc2006b90d1281
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-07-23 18:46:24 -0300

    Update Home.test.js

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,5 +1,9 @@
+import Home from '.';
+const { render } = require("@testing-library/react");
+
 describe('Testando página Home', () => {
     test('testando', () => {
+        render(<Home />);
         expect(true).toBeTruthy();
     })
 })
