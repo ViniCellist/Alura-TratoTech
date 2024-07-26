f1a6791213e8dcb1734c7848ecc76dbd7f76ab12
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-07-26 15:07:09 -0300

    feat pagamento import

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ import Anuncie from 'pages/Anuncie';
 import Carrinho from 'pages/Carrinho';
 import Categoria from 'pages/Categoria';
 import Home from 'pages/Home';
+import Pagamento from 'pages/Pagamento';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export const rotaAnuncie = 'anuncie';
-export const rotaCategoria = 'categoria/'
+export const rotaCategoria = 'categoria'
 
 export default function Router() {
   return (
@@ -16,8 +17,9 @@ export default function Router() {
           <Route index element={<Home />} />
           <Route path={`/${rotaCategoria}/:nomeCategoria`} element={<Categoria />} />
           <Route path='carrinho' element={<Carrinho />} />
+          <Route path='anuncie/:nomeCategoria' element={<Anuncie />} />
           <Route path={rotaAnuncie} element={<Anuncie />} />
-          <Route path='anuncie' element={<Anuncie />} />
+          <Route path='pagamento' element={<Pagamento />} />
         </Route>
       </Routes>
     </BrowserRouter>
