642d26b93f002f3cfc6afde3b0f80802719a04b5
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-07-26 14:32:00 -0300

    feat mock usenavigate

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,18 @@ import Categoria from 'pages/Categoria';
 import Home from 'pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+export const rotaAnuncie = 'anuncie';
+export const rotaCategoria = 'categoria/'
+
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<PaginaPadrao />}>
           <Route index element={<Home />} />
-          <Route path='/categoria/:nomeCategoria' element={<Categoria />} />
+          <Route path={`/${rotaCategoria}/:nomeCategoria`} element={<Categoria />} />
           <Route path='carrinho' element={<Carrinho />} />
-          <Route path='anuncie/:nomeCategoria' element={<Anuncie />} />
+          <Route path={rotaAnuncie} element={<Anuncie />} />
           <Route path='anuncie' element={<Anuncie />} />
         </Route>
       </Routes>
